028e1ac00afae74dccc5a040e4f5906c849871ef
SZ <sl.zheng14@gmail.com>
2017-04-01 13:59:45 +0100

    Update utils.py

--- Jay/utils.py
+++ Jay/utils.py
@@ -121,23 +121,3 @@ def fmean_squared_error(ground_truth, predictions):
     return fmean_squared_error_
 
 
-class cust_regression_vals(BaseEstimator, TransformerMixin):
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, hd_searches):
-        d_col_drops = ['id', 'relevance', 'search_term', 'product_title', 'product_description',
-                       'attr', 'brand', 'attribute', 'attr_title']
-        hd_searches = hd_searches.drop(d_col_drops, axis=1).values
-        return hd_searches
-
-
-class cust_txt_col(BaseEstimator, TransformerMixin):
-    def __init__(self, key):
-        self.key = key
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, data_dict):
-        return data_dict[self.key].apply(str)
